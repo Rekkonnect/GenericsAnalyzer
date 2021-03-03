7103614e2eee694a58665c15af1af010753e0f90
AlFas <AlFasGD@users.noreply.github.com>
2021-03-03 16:57:27 +0200

    Add pending diagnostic rule information

--- GenericsAnalyzer/GenericsAnalyzer/AnalyzerReleases.Unshipped.md
+++ GenericsAnalyzer/GenericsAnalyzer/AnalyzerReleases.Unshipped.md
@@ -1,2 +1,21 @@
 ﻿; Unshipped analyzer release
 ; https://github.com/dotnet/roslyn-analyzers/blob/master/src/Microsoft.CodeAnalysis.Analyzers/ReleaseTrackingAnalyzers.Help.md
+
+### New Rules
+
+Rule ID | Category | Severity | Notes
+--------|----------|----------|-------
+GA0002 | Validity | Error | PermittedTypeArgumentAnalyzer, [Documentation](docs/rules/GA0002.md)
+GA0003 | Validity | Error | PermittedTypeArgumentAnalyzer, [Documentation](docs/rules/GA0003.md)
+GA0004 | Validity | Error | PermittedTypeArgumentAnalyzer, [Documentation](docs/rules/GA0004.md)
+GA0005 | Validity | Error | PermittedTypeArgumentAnalyzer, [Documentation](docs/rules/GA0005.md)
+GA0006 | Brevity | Warning | PermittedTypeArgumentAnalyzer, [Documentation](docs/rules/GA0006.md)
+GA0007 | Brevity | Warning | PermittedTypeArgumentAnalyzer, [Documentation](docs/rules/GA0007.md)
+GA0008 | Brevity | Warning | PermittedTypeArgumentAnalyzer, [Documentation](docs/rules/GA0008.md)
+GA0009 | Brevity | Warning | PermittedTypeArgumentAnalyzer, [Documentation](docs/rules/GA0009.md)
+GA0010 | Brevity | Warning | PermittedTypeArgumentAnalyzer, [Documentation](docs/rules/GA0010.md)
+GA0011 | Brevity | Warning | PermittedTypeArgumentAnalyzer, [Documentation](docs/rules/GA0011.md)
+GA0012 | Validity | Error | PermittedTypeArgumentAnalyzer, [Documentation](docs/rules/GA0012.md)
+GA0013 | Design | Warning | PermittedTypeArgumentAnalyzer, [Documentation](docs/rules/GA0013.md)
+GA0015 | Brevity | Warning | PermittedTypeArgumentAnalyzer, [Documentation](docs/rules/GA0015.md)
+GA0016 | Brevity | Warning | PermittedTypeArgumentAnalyzer, [Documentation](docs/rules/GA0016.md)
--- GenericsAnalyzer/GenericsAnalyzer/DiagnosticDescriptors.cs
+++ GenericsAnalyzer/GenericsAnalyzer/DiagnosticDescriptors.cs
@@ -4,12 +4,30 @@ namespace GenericsAnalyzer
 {
     internal static class DiagnosticDescriptors
     {
+        #region Category Constants
         public const string APIRestrictionsCategory = "API Restrictions";
         public const string BrevityCategory = "Brevity";
+        public const string DesignCategory = "Design";
+        public const string ValidityCategory = "Validity";
+        #endregion
 
         #region Rules
         public static readonly DiagnosticDescriptor GA0001_Rule = GetDiagnosticDescriptor(1, APIRestrictionsCategory, DiagnosticSeverity.Error);
+        public static readonly DiagnosticDescriptor GA0002_Rule = GetDiagnosticDescriptor(2, ValidityCategory, DiagnosticSeverity.Error);
+        public static readonly DiagnosticDescriptor GA0003_Rule = GetDiagnosticDescriptor(3, ValidityCategory, DiagnosticSeverity.Error);
+        public static readonly DiagnosticDescriptor GA0004_Rule = GetDiagnosticDescriptor(4, ValidityCategory, DiagnosticSeverity.Error);
+        public static readonly DiagnosticDescriptor GA0005_Rule = GetDiagnosticDescriptor(5, ValidityCategory, DiagnosticSeverity.Error);
+        public static readonly DiagnosticDescriptor GA0006_Rule = GetDiagnosticDescriptor(6, BrevityCategory, DiagnosticSeverity.Warning);
+        public static readonly DiagnosticDescriptor GA0007_Rule = GetDiagnosticDescriptor(7, BrevityCategory, DiagnosticSeverity.Warning);
+        public static readonly DiagnosticDescriptor GA0008_Rule = GetDiagnosticDescriptor(8, BrevityCategory, DiagnosticSeverity.Warning);
+        public static readonly DiagnosticDescriptor GA0009_Rule = GetDiagnosticDescriptor(9, BrevityCategory, DiagnosticSeverity.Warning);
+        public static readonly DiagnosticDescriptor GA0010_Rule = GetDiagnosticDescriptor(10, BrevityCategory, DiagnosticSeverity.Warning);
+        public static readonly DiagnosticDescriptor GA0011_Rule = GetDiagnosticDescriptor(11, BrevityCategory, DiagnosticSeverity.Warning);
+        public static readonly DiagnosticDescriptor GA0012_Rule = GetDiagnosticDescriptor(12, ValidityCategory, DiagnosticSeverity.Error);
+        public static readonly DiagnosticDescriptor GA0013_Rule = GetDiagnosticDescriptor(13, DesignCategory, DiagnosticSeverity.Warning);
         public static readonly DiagnosticDescriptor GA0014_Rule = GetDiagnosticDescriptor(14, BrevityCategory, DiagnosticSeverity.Warning);
+        public static readonly DiagnosticDescriptor GA0015_Rule = GetDiagnosticDescriptor(15, BrevityCategory, DiagnosticSeverity.Warning);
+        public static readonly DiagnosticDescriptor GA0016_Rule = GetDiagnosticDescriptor(16, BrevityCategory, DiagnosticSeverity.Warning);
         #endregion
 
         public static string GetDiagnosticID(int id) => $"GA{id:0000}";

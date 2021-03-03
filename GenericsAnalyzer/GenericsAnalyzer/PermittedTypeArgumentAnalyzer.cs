c8c73838f0193fd4dd5222e96f8a4305aafcd06f
AlFas <AlFasGD@users.noreply.github.com>
2021-03-04 00:54:54 +0200

    Allow using type parameters in base types in more scenarios
    Probably all of them?

--- GenericsAnalyzer/GenericsAnalyzer/PermittedTypeArgumentAnalyzer.cs
+++ GenericsAnalyzer/GenericsAnalyzer/PermittedTypeArgumentAnalyzer.cs
@@ -148,10 +148,10 @@ namespace GenericsAnalyzer
                             // Recursively analyze base type definitions
                             AnalyzeGenericNameDefinition(context, inheritedType);
 
-                            var typeArguments = inheritedType.TypeArguments;
-                            for (int k = 0; k < typeArguments.Length; k++)
-                                if (typeArguments[k].Name == parameter.Name)
-                                    system.InheritFrom(genericNames[inheritedType][k]);
+                            var inheritedTypeParameters = inheritedType.TypeParameters;
+                            for (int k = 0; k < inheritedTypeParameters.Length; k++)
+                                if (inheritedTypeParameters[k].Name == parameter.Name)
+                                    system.InheritFrom(inheritedTypeParameters[k], genericNames[inheritedType][k]);
                         }
                         continue;
                     }
@@ -191,7 +191,32 @@ namespace GenericsAnalyzer
 
                 var system = constraints[i];
                 var argumentType = semanticModel.GetTypeInfo(argument).Type;
-                if (!system.IsPermitted(argumentType))
+
+                bool isPermitted = false;
+                if (argumentType is ITypeParameterSymbol declaredTypeParameter)
+                {
+                    var declaringElement = declaredTypeParameter.DeclaringMethod;
+                    ImmutableArray<ITypeParameterSymbol> declaringElementTypeParameters;
+                    if (declaringElement is null)
+                        declaringElementTypeParameters = declaredTypeParameter.DeclaringType.TypeParameters;
+                    else
+                        declaringElementTypeParameters = declaringElement.TypeParameters;
+
+                    for (int j = 0; j < declaringElementTypeParameters.Length; j++)
+                    {
+                        if (declaringElementTypeParameters[j].Name == declaredTypeParameter.Name)
+                        {
+                            isPermitted = system.IsPermitted(declaredTypeParameter, j, genericNames);
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    isPermitted = system.IsPermitted(argumentType);
+                }
+
+                if (!isPermitted)
                 {
                     var diagnostic = Diagnostic.Create(GA0001_Rule, argument.GetLocation(), originalDefinition.ToDisplayString(), argumentType.ToDisplayString());
                     context.ReportDiagnostic(diagnostic);

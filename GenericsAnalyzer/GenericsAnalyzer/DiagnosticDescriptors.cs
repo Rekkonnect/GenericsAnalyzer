8ca7a7a888525a7524c8982a5aa832f836ebae0b
AlFas <AlFasGD@users.noreply.github.com>
2021-03-15 18:13:42 +0200

    Remove GA0018
    It will be supported on a separate analyzer that's more generalized than this one

--- GenericsAnalyzer/GenericsAnalyzer/DiagnosticDescriptors.cs
+++ GenericsAnalyzer/GenericsAnalyzer/DiagnosticDescriptors.cs
@@ -28,7 +28,6 @@ namespace GenericsAnalyzer
         public static readonly DiagnosticDescriptor GA0015_Rule = GetDiagnosticDescriptor(15, BrevityCategory, DiagnosticSeverity.Warning);
         public static readonly DiagnosticDescriptor GA0016_Rule = GetDiagnosticDescriptor(16, BrevityCategory, DiagnosticSeverity.Warning);
         public static readonly DiagnosticDescriptor GA0017_Rule = GetDiagnosticDescriptor(17, APIRestrictionsCategory, DiagnosticSeverity.Error);
-        public static readonly DiagnosticDescriptor GA0018_Rule = GetDiagnosticDescriptor(18, BrevityCategory, DiagnosticSeverity.Warning);
         #endregion
 
         #region Diagnotsic Descriptor Construction

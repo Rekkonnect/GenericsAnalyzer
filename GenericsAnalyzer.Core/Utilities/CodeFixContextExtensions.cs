942f9c2309cad709b6fd09eb27d5a0bac2736b1d
AlFas <AlFasGD@users.noreply.github.com>
2021-03-20 22:13:07 +0200

    Better abstract helper functions in code fixes

---
+++ GenericsAnalyzer.Core/Utilities/CodeFixContextExtensions.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GenericsAnalyzer.Core.Utilities
+{
+    public static class CodeFixContextExtensions
+    {
+        public static async Task<Document> RemoveAttributeAsync
+        (
+            this CodeFixContext context,
+            AttributeSyntax attributeSyntax,
+            SyntaxRemoveOptions options = SyntaxRemoveOptions.KeepExteriorTrivia,
+            CancellationToken cancellationToken = default
+        )
+            => await context.Document.RemoveAttributeAsync(attributeSyntax, options, cancellationToken);
+
+        public static async Task<Document> RemoveAttributeArgumentAsync
+        (
+            this CodeFixContext context,
+            AttributeArgumentSyntax attributeArgumentSyntax,
+            SyntaxRemoveOptions options = SyntaxRemoveOptions.KeepExteriorTrivia,
+            CancellationToken cancellationToken = default
+        )
+            => await context.Document.RemoveAttributeArgumentAsync(attributeArgumentSyntax, options, cancellationToken);
+
+        public static async Task<Document> RemoveSyntaxNodeAsync
+        (
+            this CodeFixContext context,
+            SyntaxNode removedNode,
+            SyntaxRemoveOptions options = SyntaxRemoveOptions.KeepExteriorTrivia,
+            CancellationToken cancellationToken = default
+        )
+            => await context.Document.RemoveSyntaxNodeAsync(removedNode, options, cancellationToken);
+
+        public static async Task<Document> InsertSyntaxNodesAfterAsync
+        (
+            this CodeFixContext context,
+            SyntaxNode referenceNode,
+            IEnumerable<SyntaxNode> insertedNodes,
+            CancellationToken cancellationToken = default
+        )
+            => await context.Document.InsertSyntaxNodesAfterAsync(referenceNode, insertedNodes, cancellationToken);
+
+        public static async Task<Document> InsertSyntaxNodesBeforeAsync
+        (
+            this CodeFixContext context,
+            SyntaxNode referenceNode,
+            IEnumerable<SyntaxNode> insertedNodes,
+            CancellationToken cancellationToken = default
+        )
+            => await context.Document.InsertSyntaxNodesBeforeAsync(referenceNode, insertedNodes, cancellationToken);
+
+        public static async Task<SyntaxNode> GetSyntaxRootAsync(this CodeFixContext context) => await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+    }
+}
--- GenericsAnalyzer/GenericsAnalyzer.CodeFixes/MultipleDiagnosticCodeFixProvider.cs
+++ GenericsAnalyzer/GenericsAnalyzer.CodeFixes/MultipleDiagnosticCodeFixProvider.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using GenericsAnalyzer.Core.Utilities;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
@@ -28,7 +28,7 @@ namespace GenericsAnalyzer
 
         public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
         {
-            var root = await GetSyntaxRootAsync(context);
+            var root = await context.GetSyntaxRootAsync();
 
             foreach (var diagnostic in context.Diagnostics)
             {
@@ -42,86 +42,6 @@ namespace GenericsAnalyzer
                 }
             }
         }
-
-        #region Document
-        // TODO: Migrate as extensions to not require relying on this class
-        // Inconsistent signatures detected
-        protected async Task<Document> RemoveAttributeAsync(Document document, AttributeSyntax attributeSyntax, CancellationToken cancellationToken, SyntaxRemoveOptions options = SyntaxRemoveOptions.KeepExteriorTrivia)
-        {
-            SyntaxNode removedNode = attributeSyntax;
-            if ((removedNode.Parent as AttributeListSyntax).Attributes.Count == 1)
-                removedNode = removedNode.Parent;
-
-            return await RemoveSyntaxNodeAsync(document, cancellationToken, removedNode, options);
-        }
-        protected async Task<Document> RemoveAttributeArgumentAsync(Document document, AttributeArgumentSyntax attributeArgumentSyntax, CancellationToken cancellationToken, SyntaxRemoveOptions options = SyntaxRemoveOptions.KeepExteriorTrivia)
-        {
-            SyntaxNode removedNode = attributeArgumentSyntax;
-
-            if ((removedNode.Parent as AttributeArgumentListSyntax).Arguments.Count == 1)
-            {
-                removedNode = removedNode.Parent.Parent;
-                if ((removedNode.Parent as AttributeListSyntax).Attributes.Count == 1)
-                    removedNode = removedNode.Parent;
-            }
-
-            return await RemoveSyntaxNodeAsync(document, cancellationToken, removedNode, options);
-        }
-
-        protected async Task<Document> RemoveSyntaxNodeAsync(Document document, CancellationToken cancellationToken, SyntaxNode removedNode, SyntaxRemoveOptions options = SyntaxRemoveOptions.KeepExteriorTrivia)
-        {
-            var root = await document.GetSyntaxRootAsync(cancellationToken);
-            return document.WithSyntaxRoot(root.RemoveNode(removedNode, options));
-        }
-        protected async Task<Document> InsertSyntaxNodesAfterAsync(Document document, CancellationToken cancellationToken, SyntaxNode referenceNode, IEnumerable<SyntaxNode> insertedNodes)
-        {
-            var root = await document.GetSyntaxRootAsync(cancellationToken);
-            return document.WithSyntaxRoot(root.InsertNodesAfter(referenceNode, insertedNodes));
-        }
-        protected async Task<Document> InsertSyntaxNodesBeforeAsync(Document document, CancellationToken cancellationToken, SyntaxNode referenceNode, IEnumerable<SyntaxNode> insertedNodes)
-        {
-            var root = await document.GetSyntaxRootAsync(cancellationToken);
-            return document.WithSyntaxRoot(root.InsertNodesBefore(referenceNode, insertedNodes));
-        }
-        protected async Task<Document> ReplaceNodeAsync(Document document, CancellationToken cancellationToken, SyntaxNode oldNode, IEnumerable<SyntaxNode> insertedNodes)
-        {
-            var root = await document.GetSyntaxRootAsync(cancellationToken);
-            return document.WithSyntaxRoot(root.ReplaceNode(oldNode, insertedNodes));
-        }
-        protected async Task<Document> ReplaceNodeAsync(Document document, CancellationToken cancellationToken, SyntaxNode oldNode, SyntaxNode insertedNode)
-        {
-            var root = await document.GetSyntaxRootAsync(cancellationToken);
-            return document.WithSyntaxRoot(root.ReplaceNode(oldNode, insertedNode));
-        }
-
-        protected async Task<Document> InsertSyntaxNodesAfterAsync(Document document, CancellationToken cancellationToken, SyntaxNode referenceNode, params SyntaxNode[] insertedNodes)
-            => await InsertSyntaxNodesAfterAsync(document, cancellationToken, referenceNode, (IEnumerable<SyntaxNode>)insertedNodes);
-        protected async Task<Document> InsertSyntaxNodesBeforeAsync(Document document, CancellationToken cancellationToken, SyntaxNode referenceNode, params SyntaxNode[] insertedNodes)
-            => await InsertSyntaxNodesBeforeAsync(document, cancellationToken, referenceNode, (IEnumerable<SyntaxNode>)insertedNodes);
-        protected async Task<Document> ReplaceNodeAsync(Document document, CancellationToken cancellationToken, SyntaxNode oldNode, params SyntaxNode[] insertedNodes)
-            => await ReplaceNodeAsync(document, cancellationToken, oldNode, (IEnumerable<SyntaxNode>)insertedNodes);
-        #endregion
-
-        #region Context
-        protected async Task<Document> RemoveAttributeAsync(CodeFixContext context, AttributeSyntax attributeSyntax, CancellationToken cancellationToken, SyntaxRemoveOptions options = SyntaxRemoveOptions.KeepExteriorTrivia)
-            => await RemoveAttributeAsync(context.Document, attributeSyntax, cancellationToken, options);
-        protected async Task<Document> RemoveAttributeArgumentAsync(CodeFixContext context, AttributeArgumentSyntax attributeArgumentSyntax, CancellationToken cancellationToken, SyntaxRemoveOptions options = SyntaxRemoveOptions.KeepExteriorTrivia)
-            => await RemoveAttributeArgumentAsync(context.Document, attributeArgumentSyntax, cancellationToken, options);
-
-        protected async Task<Document> RemoveSyntaxNodeAsync(CodeFixContext context, CancellationToken cancellationToken, SyntaxNode removedNode, SyntaxRemoveOptions options = SyntaxRemoveOptions.KeepExteriorTrivia)
-            => await RemoveSyntaxNodeAsync(context.Document, cancellationToken, removedNode, options);
-        protected async Task<Document> InsertSyntaxNodesAfterAsync(CodeFixContext context, CancellationToken cancellationToken, SyntaxNode referenceNode, IEnumerable<SyntaxNode> insertedNodes)
-            => await InsertSyntaxNodesAfterAsync(context.Document, cancellationToken, referenceNode, insertedNodes);
-        protected async Task<Document> InsertSyntaxNodesBeforeAsync(CodeFixContext context, CancellationToken cancellationToken, SyntaxNode referenceNode, IEnumerable<SyntaxNode> insertedNodes)
-            => await InsertSyntaxNodesBeforeAsync(context.Document, cancellationToken, referenceNode, insertedNodes);
-
-        protected async Task<Document> InsertSyntaxNodesAfterAsync(CodeFixContext context, CancellationToken cancellationToken, SyntaxNode referenceNode, params SyntaxNode[] insertedNodes)
-            => await InsertSyntaxNodesAfterAsync(context, cancellationToken, referenceNode, (IEnumerable<SyntaxNode>)insertedNodes);
-        protected async Task<Document> InsertSyntaxNodesBeforeAsync(CodeFixContext context, CancellationToken cancellationToken, SyntaxNode referenceNode, params SyntaxNode[] insertedNodes)
-            => await InsertSyntaxNodesBeforeAsync(context, cancellationToken, referenceNode, (IEnumerable<SyntaxNode>)insertedNodes);
-        #endregion
-
-        protected async Task<SyntaxNode> GetSyntaxRootAsync(CodeFixContext context) => await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
 
         protected abstract Task<Document> PerformCodeFixActionAsync(CodeFixContext context, SyntaxNode syntaxNode, CancellationToken cancellationToken);
     }

458f873815d532e14cea75ee9f700146749f3668
AlFas <AlFasGD@users.noreply.github.com>
2021-08-07 15:02:39 +0300

    Add GA0023 code fix

---
+++ GenericsAnalyzer/GenericsAnalyzer.Test/PermittedTypeArguments/CodeFixes/GA0023_CodeFixTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GenericsAnalyzer.Test.PermittedTypeArguments.CodeFixes
+{
+    [TestClass]
+    public class GA0023_CodeFixTests : UngenericizerCodeFixTests
+    {
+        [TestMethod]
+        public void RedundantUsageWithCodeFix()
+        {
+            var testCode =
+@"
+[TypeConstraintProfileGroup]
+interface {|GA0023:IProfile|}<T1, T2, T3> { }
+";
+
+            var fixedCode =
+@"
+[TypeConstraintProfileGroup]
+interface IProfile { }
+";
+
+            TestCodeFixWithUsings(testCode, fixedCode);
+        }
+    }
+}

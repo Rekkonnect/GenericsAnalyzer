4b56b40e57f9839f12dbeae7ea9ab302b5db782d
AlFas <AlFasGD@users.noreply.github.com>
2021-08-16 04:40:47 +0300

    I promised overengineering and that's what we're do today

---
+++ GenericsAnalyzer.Core/Utilities/IAssociatedEnumValueAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace GenericsAnalyzer.Core.Utilities
+{
+    public interface IAssociatedEnumValueAttribute<T>
+        where T : struct, Enum
+    {
+        T AssociatedValue { get; }
+    }
+}
---
+++ GenericsAnalyzer.Core/Utilities/TypeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GenericsAnalyzer.Core.Utilities
+{
+    public static class TypeExtensions
+    {
+        public static Type GetGenericTypeDefinitionOrSame(this Type type)
+        {
+            if (!type.IsGenericType)
+                return type;
+
+            return type.GetGenericTypeDefinition();
+        }
+    }
+}
